5c6f8e32c74d69b975e3025d46e2232fac5e8939
Charles Sal <charles.asal@outlook.com>
2020-06-24 20:56:44 -0300

    initial commit

---
+++ src/store/index.js
@@ -0,0 +1,15 @@
+import { createStore, applyMiddleware } from 'redux';
+import createSagaMiddleware from 'redux-saga';
+
+import rootReducer from './modules/rootReducer';
+import rootSaga from './modules/rootSaga';
+
+const sagaMiddleware = createSagaMiddleware();
+
+const middleware = applyMiddleware(sagaMiddleware);
+
+const store = createStore(rootReducer, middleware);
+
+sagaMiddleware.run(rootSaga);
+
+export default store;
